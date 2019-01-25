b0bc86c6d783c6427583a0e96327ecaa04d99fe4
Justin Restivo <jrestivo@mit.edu>
2019-01-25 10:37:46 -0500

    initial commit

---
+++ command_for_objcopy.sh
@@ -0,0 +1,1 @@
+riscv32-unknown-linux-gnu-objcopy --update-section .apps=/tock/example_program/a.out /tock/tockos/boards/hifive1/target/riscv32imac-unknown-none-elf/release/hifive1.elf /tock/tockos/boards/hifive1/target/riscv32imac-unknown-none-elf/release/hifive1-app.elf
---
+++ gdbinit
@@ -0,0 +1,3 @@
+set riscv use_compressed_breakpoint off
+file /home/dieraca/school/fall_2018/urop/tockos/boards/hifive1/target/riscv32imac-unknown-none-elf/release/hifive1-app.elf
+target remote localhost:1234
